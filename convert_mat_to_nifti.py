2480d22ae681e2656e224bdbeb6e036bfa7374e4
tobi <tobihaui@googlemail.com>
2020-10-07 12:57:38 +0200

    Initial commit
    Convert segmentation masks from reworks.mat to NIFTI images

---
+++ convert_mat_to_nifti.py
@@ -0,0 +1,30 @@
+import os
+
+import numpy as np
+import nibabel as nib
+from scipy.io import loadmat
+
+root_mat_files = '/mnt/data/rawdata/NAKO/AT_NAKO'
+mat_file_name = 'rework.mat'
+image_size = [320, 260, 316]
+path_save_labels = '/mnt/share/rahauei1/AT_Thomas/labels'
+# Use consecutive integers for class labels, 0 is considered background
+labels = {'P_BG': 0,
+          'P_AT': 1,
+          'P_LT': 2,
+          'P_VAT': 3}
+# Iterate over subjects
+for subject in os.listdir(root_mat_files):
+    mat_content = loadmat(os.path.join(root_mat_files, subject, mat_file_name))
+    # Initialize empty image array
+    img = np.zeros(image_size)
+    for seg_class in labels:
+        seg_map = mat_content[seg_class]
+        # Swap axes from height x width x depth to width x height x depth
+        seg_map = np.swapaxes(seg_map, 0, 1)
+        # Add weighted segmentation mask to initialized image
+        img += seg_map * labels[seg_class]
+    # Create NIFTI image
+    nifti_image = nib.Nifti1Image(np.ushort(img), affine=None)
+    # Save NIFTI image as path_save_labels/AT_subject.nii.gz
+    nib.save(nifti_image, os.path.join(path_save_labels, f'AT_{subject}.nii.gz'))
