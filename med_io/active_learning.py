f4eea493673ee0500b9d1918500365ebe716125f
Christoph Staerk <christoph.d.staerk@t-online.de>
2020-11-19 12:38:31 +0100

    Code calculation of uncertainty values
    Correct/finish the function uncertainty_sampling() in active_learning.py
    that calculates an uncertainty value for a given prediction-Tensor. It
    now first calculates a value for every pixel and then averages over the
    entire image to get a value for the image.

--- med_io/active_learning.py
+++ med_io/active_learning.py
@@ -27,28 +27,25 @@ def sort_by_informativeness(dataset, config=None):
 
 def uncertainty_sampling(prediction, computation='entropy'):
     """
-        Map function for dataset, calculates an estimation of uncertainty for every
-        patch (element of dataset) and adds this value to the dataset.
-        :parm prediction: type tf.Tensor? prediction data of the patch, with
+        Calculate an estimation of uncertainty for the prediction and return
+        this value. The prediction data must be a Tensor with a probability
+        value for every class for every voxel.
+        :parm prediction: type tf.Tensor, 4D prediction data where the first 3
+        Dimensions are Space and the 4th the class probabilities
         :return: uncertainty_value
     """
-#   print(prediction.shape())
-    # from the prediction separate the predicted probabilities for each pixel by
-    # class, then average the values to get an average probability for each class
-    mean_class_probs = tf.math.reduce_mean(prediction, axis=(0, 1, 2))
-
-#     class_maps = tf.unstack(prediction, axis=-1)
-#     mean_class_probabilities = tf.constant([])
-#     for class_map in class_maps:
-#         mean_class_probs = tf.concat([tf.math.reduce_mean(class_map),
-#                                               mean_class_probs])
-
+    # Calculate an uncertainty value for every pixel producing an uncertainty-field
     if computation == 'entropy':
-        mean_class_probs_log = tf.math.log(mean_class_probs)
-        entropy = tf.math.multiply(mean_class_probs, mean_class_probs_log)
-        uncertainty = tf.math.reduce_sum(entropy)
+        # calculate the Shannon Entropy for every pixel as uncertainty value
+        probs_log = tf.math.log(prediction)
+        weighted_probs_log = tf.math.multiply(prediction, probs_log)
+        uncertainty_field = tf.math.reduce_sum(weighted_probs_log, axis=-1)
     elif computation == 'least_confident':
-        uncertainty = tf.reduce_max(mean_class_probs)
+        # pick the probability of the most likely class as uncertainty value
+        uncertainty_field = tf.reduce_max(prediction, axis=-1)
     else:
         raise Exception('Unknown way of computing the uncertainty')
+
+    # Average the values to get an average uncertainty for the entire prediction
+    uncertainty = tf.math.reduce_mean(uncertainty_field)
     return uncertainty
