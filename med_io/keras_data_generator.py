0554efdea94baff685d53a227e7c9f9ade9a9607
Christoph Staerk <christoph.d.staerk@t-online.de>
2020-12-07 22:07:35 +0100

    Introduce DataGenerator class
    Create DataGenerator class that inherits from keras.utils.Sequence and
    enables loading of data from hdf5 file in an efficient manner. The code
    is based on the blog post:
    https://stanford.edu/~shervine/
    blog/keras-how-to-generate-data-on-the-fly#
    and will be adjusted according to the situation.

--- med_io/keras_data_generator.py
+++ med_io/keras_data_generator.py
@@ -36,7 +36,7 @@ def convert_tf_records_hdf5(dataset_train_image_path, dataset_train_label_path,
         grp_id_lists = f.create_group('id_lists')
         # training data: get the data from pipeline and store as hdf5
         train_ids, val_ids = [], []
-        for img_num, (image_data, label_data) in dataset_train.take(4).enumerate(0): # faster solution?
+        for img_num, (image_data, label_data) in dataset_train.take(4).enumerate(0):  # faster solution?
             image_data = image_data.numpy()
             label_data = label_data.numpy()
             grp_images.create_dataset(str(img_num.numpy()), data=image_data)
@@ -54,4 +54,61 @@ def convert_tf_records_hdf5(dataset_train_image_path, dataset_train_label_path,
             val_ids.append(str(img_num.numpy()))
         val_ids = [s.encode('ascii') for s in val_ids]
         grp_id_lists.create_dataset('val_ids', data=val_ids)
-    return hdf5_path
+    return hdf5_path, train_ids, val_ids
+
+
+"""This code is inspired by the blog post https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly# 
+    and is an adapted version of code provided there"""
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
+                 n_classes=10, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            X[i,] = np.load('data/' + ID + '.npy')
+
+            # Store class
+            y[i] = self.labels[ID]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
