115600fbef0a4e11426ae22d2f30dc7206810f66
Christoph Staerk <christoph.d.staerk@t-online.de>
2020-12-07 22:13:01 +0100

    Adjust DataGenerator class
    Adjust the DataGenerator class to work with the data saved in the hdf5
    file according to convert_tr_record_hdf5() and the nature of the
    segmentation data (e.g. 3D label images instead of only one class value)

--- med_io/keras_data_generator.py
+++ med_io/keras_data_generator.py
@@ -62,16 +62,17 @@ def convert_tf_records_hdf5(dataset_train_image_path, dataset_train_label_path,
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
-                 n_classes=10, shuffle=True):
+
+    def __init__(self, hdf5_data_path, list_IDs, batch_size=32, dim=(32, 32, 32), n_channels=1,
+                 n_classes=4, shuffle=True):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
+        self.list_IDs = list_IDs
+        self.hdf5_data_path = hdf5_data_path
         self.on_epoch_end()
 
     def __len__(self):
@@ -81,7 +82,7 @@ class DataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Find list of IDs
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
@@ -98,17 +99,18 @@ class DataGenerator(keras.utils.Sequence):
             np.random.shuffle(self.indexes)
 
     def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
         X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
+        y = np.empty((self.batch_size, *self.dim, self.n_classes))
 
         # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            X[i,] = np.load('data/' + ID + '.npy')
+        with h5py.File(self.hdf5_data_path, 'r') as f:
+            for i, ID in enumerate(list_IDs_temp):
+                # Store sample
+                X[i,] = f['images'][ID]
 
-            # Store class
-            y[i] = self.labels[ID]
+                # Store class
+                y[i] = f['labels'][ID]
 
-        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+        return X, y
