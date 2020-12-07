a5403b28d9197158c7ff51780f999f7e26889a6a
Christoph Staerk <christoph.d.staerk@t-online.de>
2020-12-07 13:48:15 +0100

    Save data converted to hdf5 in useful structure
    Change the conversion function that converts tfRecord and saves it as
    hdf5 file, in a way so that the hdf5 file data structure is useful for
    later data reading. The data is now organized in image and label data,
    the image data is further divided into training and validation data. The
     ids of the images and accompanying labels are the same

--- med_io/keras_data_generator.py
+++ med_io/keras_data_generator.py
@@ -6,8 +6,9 @@ from pathlib import Path
 from med_io.pipeline import pipeline
 
 
-
-def convert_tf_records_hdf5(dataset_train_image_path, dataset_train_label_path, dataset_val_image_path, dataset_val_label_path, config, dataset=None):
+def convert_tf_records_hdf5(dataset_train_image_path, dataset_train_label_path,
+                            dataset_val_image_path, dataset_val_label_path,
+                            config, dataset=None):
     """ Read image and label data from TFRecords, patch the data and save it as
         hdf5 data. The process is carried out through the tf pipeline in pipeline.py"""
     # data_path_image_list = [t[i] for t in dataset_image_path for i in range(len(dataset_image_path[0]))]
@@ -21,20 +22,28 @@ def convert_tf_records_hdf5(dataset_train_image_path, dataset_train_label_path, 
     # get a dataset with the patches from TFRecords through pipeline
     dataset_train = pipeline(config, dataset_train_image_path, dataset_train_label_path,
                              dataset=dataset, no_shuffle_and_batching=True)
+    dataset_val = pipeline(config, dataset_val_image_path, dataset_val_label_path,
+                           dataset=dataset, no_shuffle_and_batching=True)
 
     # save the patches data (of the dataset) as a hdf5 file in home dir
-
     hdf5_path = Path(config['al_patches_data_dir'])
     hdf5_path.mkdir(exist_ok=True)
     hdf5_path = hdf5_path / 'al_patches_data3.hdf5'
     with h5py.File(hdf5_path, 'w') as f:
-
+        # create the hdf5 groups that store the datasets
+        grp_images_train = f.create_group('images/train')
+        grp_images_val = f.create_group('images/validation')
+        grp_labels = f.create_group('labels')
+        # training data: get the data from pipeline and store as hdf5
         for img_num, (image_data, label_data) in dataset_train.take(4).enumerate(0): # faster solution?
             image_data = image_data.numpy()
             label_data = label_data.numpy()
-            f.create_dataset((str(img_num.numpy()) + 'img'), data=image_data)
-            f.create_dataset((str(img_num.numpy()) + 'lbl'), data=label_data)
-
+            grp_images_train.create_dataset(str(img_num.numpy()), data=image_data)
+            grp_labels.create_dataset(str(img_num.numpy()), data=label_data)
+        # validation data: get the data from pipeline and store as hdf5
+        for img_num, (image_data, label_data) in dataset_val.take(4).enumerate(img_num + 1):
+            image_data = image_data.numpy()
+            label_data = label_data.numpy()
+            grp_images_val.create_dataset(str(img_num.numpy()), data=image_data)
+            grp_labels.create_dataset(str(img_num.numpy()), data=label_data)
     return hdf5_path
-
-
